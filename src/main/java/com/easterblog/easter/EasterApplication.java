6aca95ddec48e40b385f1df7869dcdfa35f6f25a
Aarón Segura <aaronulsegura@gmail.com>
2021-11-03 14:46:04 +0100

    Initial project commit

---
+++ src/main/java/com/easterblog/easter/EasterApplication.java
@@ -0,0 +1,13 @@
+package com.easterblog.easter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easterblog/easter/EasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easterblog.easter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
